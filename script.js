d32d88449ac677a86f95df14e2125e143fc47cd3
ketki kumeriya <111123802+Kumeriyakajal@users.noreply.github.com>
2023-09-11 15:01:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const byteSize = (str) => {
   // write your code here
+  // Create a Blob object from the input string
+  const blob = new Blob([str], { type: 'text/plain' });
+
+  // Get the size of the Blob in bytes
+  const sizeInBytes = blob.size;
+
+  return sizeInBytes;
+
+
+// Test cases
+//console.log(byteSize('hello world')); // Output: 11
+//console.log(byteSize('안녕하세요')); // Output: 15
+//console.log(byteSize('')); // Output: 0
+
 };
 
 // Do not change the code below
